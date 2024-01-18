490880dc44d83d5c168bf01a9f8ea5fb347d7e53
lioraft <lioraftabi@gmail.com>
2024-01-18 19:22:28 +0200

    added more tests for add book function

--- test/ac/il/bgu/qa/TestLibrary.java
+++ test/ac/il/bgu/qa/TestLibrary.java
@@ -4,7 +4,8 @@ import ac.il.bgu.qa.services.DatabaseService;
 import ac.il.bgu.qa.services.ReviewService;
 import org.junit.jupiter.api.*;
 import org.mockito.*;
-import ac.il.bgu.qa.Library;
+
+import java.lang.reflect.Method;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestLibrary {
@@ -27,18 +28,35 @@ public class TestLibrary {
         library = new Library(databaseService, reviewService);
     }
 
+    @BeforeEach
+    void setUpBook() {
+        // initialize book mock calls for valid book
+        Mockito.when(book.getISBN()).thenReturn("978-0-13-149505-0");
+        Mockito.when(book.getTitle()).thenReturn("Mocked title");
+        Mockito.when(book.getAuthor()).thenReturn("Mocked author");
+        Mockito.when(book.isBorrowed()).thenReturn(false);
+    }
+
     @Test
     void GivenBookIsNull_WhenAddBook_ThenInvalidBookException() {
         // test that an exception is thrown when trying to add a null book and the message of the exception is correct
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(null), "Invalid book.");
     }
 
-    /*
     @Test
     void GivenInvalidISBN_WhenAddBook_ThenInvalidISBNException() {
-        Mockito.when(libraryMock.isISBNValid(book.getISBN())).thenReturn(false);
+        // when isbn is called, return an invalid isbn
+        Mockito.when(book.getISBN()).thenReturn(null);
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid ISBN.");
-    } */
+        Mockito.when(book.getISBN()).thenReturn("");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid ISBN.");
+        Mockito.when(book.getISBN()).thenReturn("123");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid ISBN.");
+        Mockito.when(book.getISBN()).thenReturn("123-123");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid ISBN.");
+        Mockito.when(book.getISBN()).thenReturn("invalidISBN");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid ISBN.");
+    }
 
     @Test
     void GivenInvalidTitle_WhenAddBook_ThenInvalidTitleException() {
@@ -49,7 +67,6 @@ public class TestLibrary {
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid title.");
     }
 
-    /*
     @Test
     void GivenInvalidAuthor_WhenAddBook_ThenInvalidAuthorException() {
         // test that an exception is thrown when trying to add a book with an invalid author and the message of the exception is correct
@@ -57,7 +74,7 @@ public class TestLibrary {
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid author.");
         Mockito.when(book.getAuthor()).thenReturn("");
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Invalid author.");
-    } */
+    }
 
     @Test
     void GivenBookIsBorrowed_WhenAddBook_ThenBookIsBorrowedException() {
@@ -66,13 +83,14 @@ public class TestLibrary {
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Book with invalid borrowed state.");
     }
 
-    /*
+
     @Test
     void GivenBookAlreadyExists_WhenAddBook_ThenBookExistsException() {
-        Mockito.when(book.getISBN()).thenReturn("Mocked name");
         Mockito.when(databaseService.getBookByISBN(book.getISBN())).thenReturn(book);
         Assertions.assertThrows(IllegalArgumentException.class, () -> library.addBook(book), "Book already exists.");
-    } */
+    }
+
+
 
 
 
